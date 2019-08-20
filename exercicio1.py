1ef0ca2d348ddddb740bb6ed5b069903a901da70
Karen queiroz <kqueiroz812@gmail.com>
2019-08-20 16:27:16 -0300

    adicionar enumerate

--- exercicio1.py
+++ exercicio1.py
@@ -1,17 +1,19 @@
 def imprimir_qualquer_coisa(*args):
-    for item in args:
-        print(item)
+    for numero,item in enumerate(args): 
+        print(str(numero)+ '.' + item)
 
 imprimir_qualquer_coisa('maça', 'natação', 'pastel')
 
+# Para colocar o numero no elemento basta usar enumerate
 
-# dar print com a chave
+
+ # dar print com a chave
 
 def imprimir_qualquer_coisa(**kwargs):
-    for chave, valor in kwargs.items():
-        print(chave + ':' + valor)
+     for chave, valor in kwargs.items():
+         print(chave + ':' + valor)
 
-imprimir_qualquer_coisa(fruta='banana', nome='Karen')
+ imprimir_qualquer_coisa(fruta='banana', nome='Karen')
 
 
     
